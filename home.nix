a49f6dfab114066cb9fdb912f8a9d2a637f3d39a
mobo <morgan.bohn@pc-scol.fr>
2024-11-08 16:46:31 +0100

    add gcc

--- home.nix
+++ home.nix
@@ -36,6 +36,7 @@ in
     atuin
     starship
     # Programming languages
+    gcc
     nodejs_20
     go
     python312
@@ -101,6 +102,8 @@ in
     JENKINS_API_TOKEN = "${secrets.JENKINS_API_TOKEN}";
     # Android
     ANDROID_HOME = "/home/morgan/Android/Sdk";
+    # Libs
+    LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH";
   };
 
   home.sessionPath = [ 
